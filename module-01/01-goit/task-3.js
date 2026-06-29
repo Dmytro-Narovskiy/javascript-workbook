7a574384722c614ae7a9c68772abfe301305ced1
Dumitru Narovschi <narovskiy17@gmail.com>
2026-06-29 20:05:26 +0100

    Add GoIT Module 01 Task 3

---
+++ module-01/01-goit/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  const widthContent = Number.parseFloat(content);
+  const horizontalPadding = Number.parseFloat(padding);
+  const thicknessBorder = Number.parseFloat(border);
+  return widthContent + horizontalPadding * 2 + thicknessBorder * 2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
